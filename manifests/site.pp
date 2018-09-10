9eaf197670ef067b704658bedbc93ec658633a53
lastusrnameon3 <34868770+lastusrnameon3@users.noreply.github.com>
2018-09-09 20:39:25 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,3 @@
-#Define Default
-node "agent1.test.com" {
-
-#Config file for NTPd
-  file { "/etc/hi.txt":
-    ensure  => "present",
-    content => "hi there",
-  }
-}
 
 node "default", "agent1.test.com", {
 
